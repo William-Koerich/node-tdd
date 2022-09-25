8b2eb237f4a8226b877fddd48e5944820f0f86b3
William Koerich <williamkoerich17@gmail.com>
2022-09-25 14:08:47 -0300

    feat: created appointment entity test

---
+++ src/entities/appointment.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from 'vitest'
+import { getFutureDate } from '../tests/utils/get-future-date'
+import { Appointment } from './appointment'
+
+test('create an appointment', () => {
+    const startsAt = getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-11')
+
+    const appointment = new Appointment({
+        customer: 'William Koerich',
+        startsAt,
+        endsAt
+    })
+
+    expect(appointment).toBeInstanceOf(Appointment)
+    expect(appointment.customer).toEqual('William Koerich')
+})
+
+test('cannot create appointment with end date before start date', () => {
+    const startsAt = getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-09')
+
+   expect(() => {
+       return new Appointment({
+        customer: 'William Koerich',
+        startsAt,
+        endsAt
+    })
+   }).toThrow()
+})
+
+test('cannot create appointment with start date before now', () => {
+    const startsAt = new Date()
+    const endsAt = new Date()
+    
+    startsAt.setDate(startsAt.getDate() -1)
+    endsAt.setDate(endsAt.getDate() + 3)
+
+   expect(() => {
+       return new Appointment({
+        customer: 'William Koerich',
+        startsAt,
+        endsAt
+    })
+   }).toThrow()
+})
