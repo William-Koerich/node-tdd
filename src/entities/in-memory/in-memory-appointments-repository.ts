ba20c1ae9716d44fc358845b00f210876c7ddd0c
William Koerich <williamkoerich17@gmail.com>
2022-09-25 14:13:29 -0300

    feat: created in memory appointments repository

---
+++ src/entities/in-memory/in-memory-appointments-repository.ts
@@ -0,0 +1,28 @@
+import { areIntervalsOverlapping} from 'date-fns'
+
+import { AppointmentRepository } from "../../repositories/appointments-repository";
+import { Appointment } from "../appointment";
+
+export class InMemoryAppointmentsRepository implements AppointmentRepository {
+    public items: Appointment[] = []
+
+    async create(appointment: Appointment): Promise<void> {
+        this.items.push(appointment)
+    }
+
+    async findOverlappingAppointment(startsAt: Date, endsAt: Date): Promise<Appointment | null> {
+        const overlappingAppointment = this.items.find(appointment => {
+            return areIntervalsOverlapping(
+                {start: startsAt, end: endsAt},
+                {start: appointment.startsAt, end: appointment.endsAt},
+                {inclusive: true}
+            )
+        })
+
+        if(!overlappingAppointment) {
+            return null
+        }
+        
+        return overlappingAppointment
+    }
+}
